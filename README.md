f1cf41a17c987736dc3b08707814fb214738f6a3
metravod <arttrek42@gmail.com>
2023-02-11 12:20:52 +0300

    description

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# convert_df
+# ConvertDF
+
+A small class that will help you make the pandas dataframe serialization process more controllable.
+
+```python
+data = [[12, 'foo', 567.53], [45, 'bar', 421.3], [35, 'baz', 1321.5]]
+cols = ['int_col', 'str_col', 'float_col']
+df = pd.DataFrame(data, columns=cols)
+
+ConvertDF(
+    df.loc[:0], 
+    rename_cols={'int_col': 'ids', 'str_col': 'type', 'float_col': 'value'}, 
+    type_cols={'int_col': str, 'float_col': int},
+    is_oneliner=True
+).to_dict()
+
+>>> {'ids': '12', 'type': 'foo', 'value': 567}
+```
--- convert_df.py
+++ convert_df.py
@@ -4,6 +4,12 @@ from typing import List, Dict, Tuple, Any, Union, Optional
 
 
 class ConvertDF:
+    """
+    A class for more controlled serialization of a pandas dataframe.
+
+    It is possible to choose, rename required columns, change the type of columns,
+    select the saving option - as a dictionary or a list of dictionaries.
+    """
 
     def __init__(
             self,
@@ -31,7 +37,7 @@ class ConvertDF:
         return range(0, len(self._input_df))
 
     def _define_final_cols(self) -> None:
-        """Финальные ключи с учетом переименования"""
+        """Final dictionary with new name for columns"""
         if self._cols and self._rename_cols:
             self._final_cols = {
                 col: self._rename_cols[col]
@@ -40,12 +46,18 @@ class ConvertDF:
             }
         elif self._cols:
             self._final_cols = {col: col for col in self._cols}
+        elif self._rename_cols:
+            self._final_cols = {
+                col: self._rename_cols[col]
+                if col in self._rename_cols.keys() else col
+                for col in self._input_df.columns
+            }
         else:
             self._final_cols = {col: col for col in self._input_df.columns}
         self._cols = [col for col in self._final_cols.keys()]
 
     def _get_value(self, row: Tuple[Any], col: str) -> Any:
-        """Забираем из нужного столбца значение и приводим к нужному типу"""
+        """Get and cast value from required column"""
         type_value = self._define_type_col(self._type_cols, col)
         value = getattr(row, col) if pd.notna(getattr(row, col)) else None
         return type_value(value) if type_value and value else value
@@ -64,7 +76,8 @@ class ConvertDF:
 
     def to_dict(self) -> Union[Dict[str, Any], List[Dict[str, Any]], None]:
         return (
-            {self._final_cols[col]: None for col in self._cols} if self._is_df_empty()
+            {self._final_cols[col]: None for col in self._cols}
+            if self._is_df_empty()
             else self._define_final_result([
                 {self._final_cols[col]: self._get_value(row, col) for col in self._cols}
                 for row in self._input_df[self._cols].itertuples(index=False)
@@ -73,7 +86,8 @@ class ConvertDF:
 
     def to_list(self) -> Union[List[Any], List[List[Any]], None]:
         return (
-            [None for col in self._cols] if self._is_df_empty()
+            [None for col in self._cols]
+            if self._is_df_empty()
             else self._define_final_result([
                 [self._get_value(row, col) for col in self._cols]
                 for row in self._input_df[self._cols].itertuples(index=False)
